71b089c66671d1e4623d2f5f1ab04f39f27de552
Kris13081 <kristianstefanov0@gmail.com>
2023-10-27 22:55:48 +0300

    Application created

---
+++ src/main/java/uni/graduate/fitwiz/FitWizApplication.java
@@ -0,0 +1,13 @@
+package uni.graduate.fitwiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitWizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitWizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/graduate/fitwiz/FitWizApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.graduate.fitwiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitWizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
