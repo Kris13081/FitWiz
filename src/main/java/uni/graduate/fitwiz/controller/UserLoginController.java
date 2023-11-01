40452feacd78681ad7ef6fda9626275cbc8da891
Kris13081 <kristianstefanov0@gmail.com>
2023-11-01 22:08:47 +0200

    login and reg form was created

---
+++ src/main/java/uni/graduate/fitwiz/controller/UserLoginController.java
@@ -0,0 +1,16 @@
+package uni.graduate.fitwiz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/login")
+public class UserLoginController {
+
+    @GetMapping()
+    public ModelAndView getLoginPage() {
+        return new ModelAndView("loginAndRegistration");
+    }
+}
