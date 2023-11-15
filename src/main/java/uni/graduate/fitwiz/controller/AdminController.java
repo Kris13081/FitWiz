f9ca1c42a9df3495436aa272448700f109635f49
Kris13081 <kristianstefanov0@gmail.com>
2023-11-15 22:39:29 +0200

    .

--- src/main/java/uni/graduate/fitwiz/controller/AdminController.java
+++ src/main/java/uni/graduate/fitwiz/controller/AdminController.java
@@ -1,16 +1,40 @@
 package uni.graduate.fitwiz.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import uni.graduate.fitwiz.service.UserService;
 
 @RestController
 @RequestMapping("/api/admins")
 public class AdminController {
 
-    @GetMapping("/menu")
-    public ModelAndView getAdminMenuPage(){
-        return new ModelAndView( "/admin/admin-menu");
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @ModelAttribute(name = "userDetails")
+    public void getUserDetails(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+
+        model.addAttribute("userDetails", userService.getUserDetails(currentUsername));
+    }
+
+    @GetMapping("/index")
+    public ModelAndView getAdminMenuPage() {
+        return new ModelAndView("/admin/index");
+    }
+
+    @GetMapping("/pages/widgets")
+    public ModelAndView getAdminWidgetsPage() {
+        return new ModelAndView("/admin/pages/widgets");
     }
 }
