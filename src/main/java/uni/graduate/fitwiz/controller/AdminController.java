7cab9eeba64c4b276e141ee3a405ac5369ba8274
Kris13081 <kristianstefanov0@gmail.com>
2023-11-19 09:48:12 +0200

    Added a new page in the admin menu for creating products.

--- src/main/java/uni/graduate/fitwiz/controller/AdminController.java
+++ src/main/java/uni/graduate/fitwiz/controller/AdminController.java
@@ -33,8 +33,8 @@ public class AdminController {
         return new ModelAndView("/admin/index");
     }
 
-    @GetMapping("/pages/widgets")
+    @GetMapping("/add-product")
     public ModelAndView getAdminWidgetsPage() {
-        return new ModelAndView("/admin/pages/widgets");
+        return new ModelAndView("/admin/add-product");
     }
 }
