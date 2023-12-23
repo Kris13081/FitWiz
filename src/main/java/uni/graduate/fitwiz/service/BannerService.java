5bead50463a170265179022afffd6060f2ba589c
Kris13081 <kristianstefanov0@gmail.com>
2023-12-24 01:32:35 +0200

    Fixed more of the direct data exposing related with the banners

--- src/main/java/uni/graduate/fitwiz/service/BannerService.java
+++ src/main/java/uni/graduate/fitwiz/service/BannerService.java
@@ -2,6 +2,7 @@ package uni.graduate.fitwiz.service;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import uni.graduate.fitwiz.model.dto.BannerDisplayDto;
 import uni.graduate.fitwiz.model.dto.BannerEntityDto;
 import uni.graduate.fitwiz.model.dto.BannerUpdateDto;
 import uni.graduate.fitwiz.model.entity.BannerEntity;
@@ -12,7 +13,7 @@ import java.util.List;
 public interface BannerService {
     ResponseEntity<String> create(BannerEntityDto bannerEntityDto) throws IOException;
 
-    List<BannerEntity> getBanners();
+    List<BannerDisplayDto> getBanners();
 
     void delete(Long id);
 
